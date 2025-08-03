80e1c3c6fb10beb7b8b519ca82da3ab6291c3b5f
Vitalii Yakubovskyi <vitaliiyakubovskyi@gmail.com>
2025-08-04 00:28:02 +0200

    imp1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Menu, X, Car } from "lucide-react";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { Language } from "../types";
+import logoImage from "../img/logo1.png"; //
 
 interface HeaderProps {
   currentLanguage: Language;
@@ -28,19 +29,13 @@ const Header: React.FC<HeaderProps> = ({
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-gray-900/95 backdrop-blur-sm shadow-2xl border-b border-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-25">
           <div className="flex items-center space-x-2">
-            {/* <div className="w-8 h-8 bg-gradient-to-r from-cyan-400 to-blue-500 rounded-lg flex items-center justify-center shadow-lg">
-              <Car className="w-5 h-5 text-white" />
-            </div> */}
-            {/* <img className="text-xl font-bold text-white">WitalCar</span> */}
-
             <img
-              src="../img//logo.jpeg"
+              src={logoImage}
               alt="Logo WICAR"
-              width="80"
-              height="70"
-            ></img>
+              className="w-40 h-auto" // Responsywne wymiary
+            />
           </div>
 
           {/* Desktop Navigation */}
